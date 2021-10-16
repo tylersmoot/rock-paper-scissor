eee1253a2b5973820a1452c8bf395aa1f1563a82
Tyler <tylercsmoot8@gmail.com>
2021-10-16 17:22:02 -0500

    script.js

--- script.js
+++ script.js
@@ -1,42 +1,41 @@
 const options = ["rock", "paper", "scissors"];
+let userSelection = "paper";
 
 
-
+let computerAnswer = computerPlay();
 
 function computerPlay () {
 
     let computerAnswer = Math.floor(Math.random() * options.length);
-    let index = computerAnswer;
+    // let index = computerAnswer;
 
     
     return options[computerAnswer].toUpperCase();
-    computerAnswer = options[computerAnswer];   /*  testing this option  */
+    computerAnswer = options[computerAnswer];    
         
     };
 
+    function playRound(userSelection, computerAnswer) {
+        console.log(computerAnswer)
+    }
 
-
-
-
-
-
-function userPlay (arg) {
-
-
-for (let i = 0; i < options.length; i++) {
     
-let userAnswer = arg;
-if (userAnswer !== options[i]) {
-    return "Not an anwswer";
-}
 
 
-return userAnswer.toUpperCase();
+   
+   
+    
 
 
-}
 
-};
+
+
+
+
+
+
+
+
 
 
 
