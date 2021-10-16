c8e5fb8229660271421ecf0561e41771139ec0f6
Tyler <tylercsmoot8@gmail.com>
2021-10-16 18:54:30 -0500

    script.js

--- script.js
+++ script.js
@@ -1,41 +1,42 @@
 const options = ["rock", "paper", "scissors"];
 let userSelection = "paper";
-
-
 let computerAnswer = computerPlay();
+
+
 
 function computerPlay () {
 
     let computerAnswer = Math.floor(Math.random() * options.length);
-    // let index = computerAnswer;
-
     
-    return options[computerAnswer].toUpperCase();
+    
+    return options[computerAnswer];
     computerAnswer = options[computerAnswer];    
         
     };
 
+   
     function playRound(userSelection, computerAnswer) {
-        console.log(computerAnswer)
-    }
+        
+        if (userSelection === "paper" && computerAnswer !== "scissors") {
+            console.log("you win");
+            console.log(userSelection + "your answer");
+            console.log(computerAnswer + "computer answer");
+        }
+            else if (userSelection ==="rock" && computerAnswer !== "paper") {
+                console.log("computer wins")
+            }
+
+            else if (userSelection === "scissors" && computerAnswer !== "rock") {
+                console.log("you win");
+            }
+            
+            
+        } 
+    
+    
+    
 
     
 
 
    
-   
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
